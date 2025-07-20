c34439ae78819a8e0d7acbe3af3b6cb0e68956c6
Branko Petrevski <bpetrevski2@gmail.com>
2025-07-20 18:09:26 +0200

    fixing react-scripts and App display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 import './App.css';
-import MapMKD from './components/map/MapMKD';
+import MapMKD from './components/map/MapMKD.jsx';
 
 function App() {
   return (
     <>
-      <MapMKD />
+      <header className="App-header">
+        <h1>Macedonian Folklore App</h1>
+      </header>
+      <main>
+        <p>Explore the rich folklore of Macedonia through this interactive map.</p>
+      </main>
+      <footer className="App-footer">
+        <p>&copy; 2023 Macedonian Folklore App</p>
+      </footer>
+      {/* Render the map component */}
+      <div className="map-container">
+        <MapMKD />
+      </div>
     </>
   );
 }
