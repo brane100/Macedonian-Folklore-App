7e74d3ec80ef1bfe35e8b5045ef4302fecfcc279
Branko Petrevski <bpetrevski2@gmail.com>
2025-08-10 23:01:13 +0200

    edit backend url

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 4445;
 
 // Middleware - CORS configuration for authentication
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], // Frontend URLs
+  origin: [`${process.env.FRONTEND_URL}`, `http://127.0.0.1:${PORT}`], // Frontend URLs
   methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE'],
   credentials: true, // Allow cookies/sessions
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
