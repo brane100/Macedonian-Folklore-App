91127f568221512a9361c717978c823e73ac4833
Branko Petrevski <89221032@student.upr.si>
2025-08-18 22:43:08 +0200

    fix file path

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 /////////////////////////////////////////////////////////////
-require('./keepalive.js'); // Keep the server alive
+require('./routes/keepalive.js'); // Keep the server alive
 
 const express = require('express');
 const cors = require('cors');
